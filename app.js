6871bacf84e11295d5bf4424be189367c03dd285
Elvie Ntumba <ntumbaelvie@gmail.com>
2025-04-16 20:38:22 +0200

    my changes

---
+++ app.js
@@ -0,0 +1,12 @@
+
+  new Swiper('.swiper-wrapper', {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
